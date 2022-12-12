998ade46017e2c0346f2332e5f078e289a3e6f97
SeanDotExe <s.monacillo59@gmail.com>
2022-12-12 12:14:27 +0800

    last na hahaha

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     trailingSlash: true,
     basePath: process.env.NODE_ENV === 'production' ? '/sakai-react' : '',
     publicRuntimeConfig: {
-        contextPath: process.env.NODE_ENV === 'production' ? '/sakai-react' : '',
+        contextPath: process.env.NODE_ENV === 'production' ? '/sakai-react/' : '',
         uploadPath: process.env.NODE_ENV === 'production' ? '/sakai-react/upload.php' : '/api/upload'
     }
 };
