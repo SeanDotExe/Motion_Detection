eca95e7119b7c4cf1bba798654e7a7ca38cc7cef
SeanDotExe <s.monacillo59@gmail.com>
2022-12-12 13:12:07 +0800

    change footer

--- layout/AppFooter.js
+++ layout/AppFooter.js
@@ -1,14 +1,16 @@
 import getConfig from 'next/config';
 import React, { useContext } from 'react';
+import { Image } from 'primereact/image';
 import { LayoutContext } from './context/layoutcontext';
 
 const AppFooter = () => {
     const { layoutConfig } = useContext(LayoutContext);
     const contextPath = getConfig().publicRuntimeConfig.contextPath;
+    
 
     return (
         <div className="layout-footer">
-            <img src={require("/demo/images/galleria/Frame 1team.png")} alt="" width={1100}/>
+            <img src={`${contextPath}/demo/images/galleria/Frame 1team.png`} alt="Team" width={1100}/>
             {/* <img src={`${contextPath}/layout/images/logo-${layoutConfig.colorScheme === 'light' ? 'dark' : 'white'}.svg`} alt="Logo" height="20" className="mr-2" /> */}
             {/* <span className="font-medium ml-2">Motion Detection</span> */}
         </div>
