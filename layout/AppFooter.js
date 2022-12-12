a1f14a617e36bd1214c16ae3ca365a9b084ebbf3
SeanDotExe <s.monacillo59@gmail.com>
2022-12-12 12:35:40 +0800

    add pics

--- layout/AppFooter.js
+++ layout/AppFooter.js
@@ -8,7 +8,7 @@ const AppFooter = () => {
 
     return (
         <div className="layout-footer">
-            <img src="/layout/images/Frame 1team.png" alt="galleria" width={1100}/>
+            <img src="/demo/images/galleria/Frame 1team.png" alt="Team" width={1100}/>
             {/* <img src={`${contextPath}/layout/images/logo-${layoutConfig.colorScheme === 'light' ? 'dark' : 'white'}.svg`} alt="Logo" height="20" className="mr-2" /> */}
             {/* <span className="font-medium ml-2">Motion Detection</span> */}
         </div>
